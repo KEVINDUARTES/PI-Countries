c252d9c9a890061d0c43c8d78c517756ffc11610
facundo <axel_facundo9@hotmail.com>
2022-07-11 18:05:41 -0300

    facu commit

--- api/src/Controller/getCountries.js
+++ api/src/Controller/getCountries.js
@@ -1,47 +1,44 @@
-const axios = require ('axios');
-const {Country, Activity }= require('../../db');
-const {URL_COUNTRIES}=process.env
+const axios = require("axios");
+const { Country, Activity } = require("../db");
+const { URL_COUNTRIES } = process.env;
 
 const getApiInfo = async () => {
-    const apiUrl = await axios.get(URL_COUNTRIES);
- console.log("api-Url", apiUrl);
-const apiInfo= await apiUrl.data.map(el =>{
-    return{
-        id: el.cca3,
-        nombre: el.nombre,
-        bandera: el.bandra ,
-        continente: el.continente,
-        capital: el.capital ? el.capital[0] : 'Capital no encontrada',
-        subregion: el.subregion ? el.subregion : 'Subregion no encontrado',
-        area: el.area,
-        poblacion: el.poblacion,
+  const apiUrl = await axios.get(URL_COUNTRIES);
+  console.log("api-Url", apiUrl);
+  const apiInfo = await apiUrl.data.map((el) => {
+    return {
+      id: el.cca3,
+      nombre: el.nombre,
+      bandera: el.bandra,
+      continente: el.continente,
+      capital: el.capital ? el.capital[0] : "Capital no encontrada",
+      subregion: el.subregion ? el.subregion : "Subregion no encontrado",
+      area: el.area,
+      poblacion: el.poblacion,
     };
-});
+  });
 
-return apiInfo;//retorno la info que le pedi
-
+  return apiInfo; //retorno la info que le pedi
 };
 const getDbInfo = async () => {
-    return await Country.findAll({
-        include: {
-            model: Activity,
-            atributos: ["nombre", "dificultad", "duracion", "temporada"],
-            through: {
-                atributos: [],
-            },
-        },
-    })
-}
-
-
+  return await Country.findAll({
+    include: {
+      model: Activity,
+      atributos: ["nombre", "dificultad", "duracion", "temporada"],
+      through: {
+        atributos: [],
+      },
+    },
+  });
+};
 
 const getAllCountries = async () => {
-    const apiInfo = await getApiInfo();
-    const dbInfo = await getDbInfo();
-    const infoTotal = apiInfo.concat(dbInfo);
-    return infoTotal
- }
+  const apiInfo = await getApiInfo();
+  const dbInfo = await getDbInfo();
+  const infoTotal = apiInfo.concat(dbInfo);
+  return infoTotal;
+};
 
- module.exports={
-    getAllCountries,
- }
+module.exports = {
+  getAllCountries,
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,16 @@
-const { Router } = require('express');
-const countryRoute = require ('./countryRoutes.js')
-const actividadesRoute = requiere ('./actividadesRoutes.js')
+const { Router } = require("express");
+const countryRoute = require("./countryRoutes.js");
+const actividadesRoute = require("./actividadesRoutes");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use('/country', countryRoute)
-router.use('/activity', actividadesRoute)
+router.use("/country", countryRoute);
+router.use("/activity", actividadesRoute);
 
 module.exports = router;
